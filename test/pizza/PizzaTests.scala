e13c0a2e0e3b6b0d17a534ef9d1d3ebc6821e487
Jouni Leino <jouni.leino@siili.com>
2016-08-27 00:53:47 +0300

    Travis build badge

---
+++ test/pizza/PizzaTests.scala
@@ -0,0 +1,25 @@
+package pizza
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class PizzaTests extends FunSuite with BeforeAndAfter {
+
+  var pizza: Pizza = _
+
+  before {
+    pizza = new Pizza
+  }
+
+  test("new pizza has zero toppings") {
+    assert(pizza.getToppings.size == 0)
+  }
+
+  test("adding one topping") {
+    pizza.addTopping(Topping("green olives"))
+    assert(pizza.getToppings.size === 1)
+  }
+
+  // mark that you want a test here in the future
+  test ("test pizza pricing") (pending)
+
+}
